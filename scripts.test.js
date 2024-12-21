10713f6341bcee28acea3c24873095d31410fe9a
nainsworth <nja8161995@gmail.com>
2024-12-20 20:03:27 -0600

    Added reverse string function

--- scripts.test.js
+++ scripts.test.js
@@ -1,5 +1,10 @@
 const capitalize = require("./js/capitalize");
+const reverseString = require("./js/reverse");
 
 test("capitalizes string", () => {
   expect(capitalize("Capitalize this string")).toBe("CAPITALIZE THIS STRING");
 });
+
+test("reverse string", () => {
+    expect(reverseString("Reverse this string")).toBe("gnirts siht esreveR");
+})
